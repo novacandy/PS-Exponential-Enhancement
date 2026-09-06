8099704daa40ba081e01bcb1066cf601ed0f7635
Nova <140567692+novacandy@users.noreply.github.com>
2026-09-06 18:44:12 -0500

    prevent crash when erin dialogue line is undefined

--- js/furtherEnhance.js
+++ js/furtherEnhance.js
@@ -772,9 +772,9 @@ addLayer("f", {
         erin: {
             title: "Erin",
             body() {return `
-                    <br><img src='resources/erinPortraits/${(furtherDialogue[dialogueID].dialogue[dialogueLine].erinPortrait !== undefined ? furtherDialogue[dialogueID].dialogue[dialogueLine].erinPortrait : "erinNormal") + (dialogueStep > furtherDialogue[dialogueID].dialogue[dialogueLine].erinText().length ? "Still" : "")}.png' width='305' height='363'><br>
+                    <br><img src='resources/erinPortraits/${furtherDialogue[dialogueID].dialogue[dialogueLine] !== undefined ? (furtherDialogue[dialogueID].dialogue[dialogueLine].erinPortrait + (dialogueStep > furtherDialogue[dialogueID].dialogue[dialogueLine].erinText().length ? "Still" : "")) : "erinNormalStill"}.png' width='305' height='363'><br>
                     <br><p>${renderDialogue(Math.floor(dialogueStep))}</p><br>
-                    <p>> ${furtherDialogue[dialogueID].dialogue[dialogueLine].command !== undefined ? furtherDialogue[dialogueID].dialogue[dialogueLine].command : ""}</p><br>
+                    <p>> ${furtherDialogue[dialogueID].dialogue[dialogueLine] !== undefined ? (furtherDialogue[dialogueID].dialogue[dialogueLine].command !== undefined ? furtherDialogue[dialogueID].dialogue[dialogueLine].command : "") : ""}</p><br>
             `},
             unlocked() {return true}
         },
